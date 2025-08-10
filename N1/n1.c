bfb27ce8743e3eb90af2b88b1c6eb19a598fba82
thainabteodoro <thainabteodoro@outlook.com>
2025-08-10 15:37:20 +0000

    chmod +x n1

---
+++ N1/n1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 26;
+    float altura = 1.61;
+    char opcao = 's';
+    char nome[20] = "thaina"; 
+  
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Opcao: %c\n", opcao);
+    printf("Nome: %s\n", nome);
+
+    return 0;
